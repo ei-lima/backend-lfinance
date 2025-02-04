8d0e328d5f4153233e4be34698231ac16094b899
ei-lima <guilherme@cpninformatica.com.br>
2025-02-04 00:34:03 -0300

    add log on login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController.js');
+const logMiddleware = require('../middlewares/logMiddleware.js');
 
+router.use(logMiddleware)
 // no auth
 router.post('/login', userController.authUser);
 
